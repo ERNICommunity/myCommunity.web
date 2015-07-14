7208418fd5df45fed7c35a58322c959b55ff0fac
Fabian Schneiter <fabian.schneiter@erni.ch>
2015-07-14 15:09:45 +0200

    Missing changes
    wtf GIT

---
+++ app/modules/contentAdmin/navButton/adminNavButton.controller.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('myCommunityApp.contentAdmin');
+    module.controller('AdminNavButtonController', ['$scope', 'YammerLoginData', AdminNavButtonController ]);
+
+    function AdminNavButtonController($scope, YammerLoginData) {
+        $scope.loginData = YammerLoginData; // copy reference to access object, don't assign value
+   }
+})();
