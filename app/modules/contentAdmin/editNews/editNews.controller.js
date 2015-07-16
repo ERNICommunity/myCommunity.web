64f017a7077ba0986d7d31dff769af89d94f1ce2
Fabian Schneiter <fabian.schneiter@erni.ch>
2015-07-16 10:25:54 +0200

    Made EditNews and CreateNewNewsItem distinguishable
    EditNewsController now serves $scope.isNewItem

--- app/modules/contentAdmin/editNews/editNews.controller.js
+++ app/modules/contentAdmin/editNews/editNews.controller.js
@@ -4,17 +4,19 @@
     var module = angular.module('myCommunityApp.contentAdmin');
     module.controller('EditNewsController', function ($stateParams, $scope) {
 
-        var newsItemToBeEdited = $scope.news.filter(function (elem) {
-            return elem.id === $stateParams.id;
-        })[0];
+        var newsItemId = $stateParams.id;
+        $scope.isNewItem = newsItemId == 'new';
 
-        console.dir(newsItemToBeEdited);
+        if ($scope.isNewItem) {
+            // create picture urls to populate dynamic field list in form
+            $scope.newsItem = { pictureurls: [ '' ] }
+        } else {
+            var selectedNewsItem = $scope.news.filter(function (elem) { return elem.id === newsItemId; })[0];
+            var newsItemCopy = jQuery.extend(true, {}, selectedNewsItem); // create a copy to allow undo.
+            $scope.newsItem = newsItemCopy;
+    }
 
-        // create a copy to allow undo.
-        var newsItemCopy = jQuery.extend(true, {}, newsItemToBeEdited);
-        $scope.newsItem = newsItemCopy;
-
-        $scope.isLastPictureUrl = function(index) {
+        $scope.isLastPictureUrl = function (index) {
             return index == $scope.newsItem.pictureurls.length - 1;
         }
     });
