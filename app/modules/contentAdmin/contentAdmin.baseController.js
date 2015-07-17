c331e538b7ab9f41c5739bff501fe95bb52ec2ed
Fabian Schneiter <fabian.schneiter@erni.ch>
2015-07-17 12:49:07 +0200

    Hide events and news of other users
    i.e. Items you won't be able to edit.
    Exceptions: Jazz, Daniela and me can see all the events (the filtering
    is just disabled, frontend-wise)

--- app/modules/contentAdmin/contentAdmin.baseController.js
+++ app/modules/contentAdmin/contentAdmin.baseController.js
@@ -4,27 +4,45 @@
     // Controller for the empty ui-view (declared in route.js):
     // Sets up the parent-scope with things that are used globally in the admin-content views
     angular.module('myCommunityApp.contentAdmin')
-        .controller('ContentAdminBaseController', function ($scope, $state, YammerLoginData, events, news) {
+        .controller('ContentAdminBaseController', ContentAdminBaseController);
 
-            // share across child views
+    var usersThatCanSeeAllEvents = [
+        "Fabian Schneiter",
+        "Jazz Kang",
+        "Daniela Moser"];
+
+    function ContentAdminBaseController($scope, $state, $filter, YammerLoginData, events, news) {
+
+        $scope.loginData = YammerLoginData;
+
+        console.dir(YammerLoginData);
+
+        // add news and events available to all child scopes (edit forms etc.)
+        if (userCanSeeAllItems(YammerLoginData)) {
             $scope.events = events;
             $scope.news = news;
-            $scope.loginData = YammerLoginData;
+        }
+        else {
+            $scope.events = $filter('filter')(events, {organiser: YammerLoginData.userName}, true);
+            $scope.news = $filter('filter')(news, {author: YammerLoginData.userName}, true);
+        }
 
-            if (!YammerLoginData.isLoggedIn)
-            {
+        if (!YammerLoginData.isLoggedIn) {
+            $state.go('calendar.list');
+        }
+
+        // make sure we move away from our pages on logout
+        $scope.$on('YammerUserLogout', onUserLogout)
+        function onUserLogout() {
+            console.info('ContentAdminController: onUserLogout, navigate away from admin page.')
+            if ($state.includes('contentAdmin')) {
                 $state.go('calendar.list');
             }
+        }
+    };
 
-            // make sure we move away from our pages on logout
-            $scope.$on('YammerUserLogout', onUserLogout)
-            function onUserLogout() {
-                console.info('ContentAdminController: onUserLogout, navigate away from admin page.')
-                if ($state.includes('contentAdmin')) {
-                    $state.go('calendar.list');
-                }
-            }
+    function userCanSeeAllItems(loginData) {
+        return (usersThatCanSeeAllEvents.indexOf(loginData.userName) > -1);
+    };
 
-
-        });
 })();
